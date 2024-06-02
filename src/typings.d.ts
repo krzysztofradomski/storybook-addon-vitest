fcb3e809a03e0cf1707d9858f8d8f592e5fea502
Ahmed Jaouadi <d504280@burda.com>
2024-06-02 05:10:39 +0200

    Format code

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,49 +1,49 @@
 declare module "global";
 
 export type VitestParams = {
-  testFile: string
-  testResults: JSONTestResults
-}
+  testFile: string;
+  testResults: JSONTestResults;
+};
 
 export type JSONTestResults = {
-  numTotalTestSuites: number
-  numPassedTestSuites: number
-  numFailedTestSuites: number
-  numPendingTestSuites: number
-  numTotalTests: number
-  numPassedTests: number
-  numFailedTests: number
-  numPendingTests: number
-  numTodoTests: number
-  startTime: number
-  success: boolean
-  testResults: TestResult[]
-}
+  numTotalTestSuites: number;
+  numPassedTestSuites: number;
+  numFailedTestSuites: number;
+  numPendingTestSuites: number;
+  numTotalTests: number;
+  numPassedTests: number;
+  numFailedTests: number;
+  numPendingTests: number;
+  numTodoTests: number;
+  startTime: number;
+  success: boolean;
+  testResults: TestResult[];
+};
 
 export type TestResult = {
-  assertionResults: Array<AssertionResult>
-  startTime: number
-  endTime: number
-  status: string
-  message: string
-  name: string
-}
+  assertionResults: Array<AssertionResult>;
+  startTime: number;
+  endTime: number;
+  status: string;
+  message: string;
+  name: string;
+};
 
 export type AssertionResult = {
-  ancestorTitles: [string, string]
-  fullName: string
-  title: string
-  duration: number
-  status: string
-}
+  ancestorTitles: [string, string];
+  fullName: string;
+  title: string;
+  duration: number;
+  status: string;
+};
 
 export type Result = {
-  title: string
-  status: string
-}
+  title: string;
+  status: string;
+};
 
-export type GroupResult = Array<Result>
+export type GroupResult = Array<Result>;
 
 export type GroupedTestResults = {
-  [key: string]: GroupResult
-}
+  [key: string]: GroupResult;
+};
